1919475672018e4027fc6700e517e12c5c408763
KingCodeFish <kingcodefish@gmail.com>
2016-01-11 20:43:19 -0500

    Update game.js

--- Game/game.js
+++ Game/game.js
@@ -35,21 +35,59 @@ _  ____/_  /   / /_/ / /_/ / / /_/ // /__ / /_ _  / / /_/ /  / / /(__  )
 
 /**
  * 
- * Some random stuff about the game here
+ * To get the most out of this game, install the Star Jedi Rounded font here: http://www.fontspace.com/category/star%20wars
  * 
  */
+
+var keys = [];
+var mouseOverButton = "";
+
+var button = function(x, y, wid, hei, radius, tex, textSi, textFill, hoverTextFill, fi, hoverFi, stro, stroWei, hoverStro, hoverStroWei) {
+    if(abs(x - mouseX) <= wid / 2 && abs(y - mouseY) <= hei / 2) {
+        if(mouseIsPressed) {
+            mouseOverButton = tex;
+        }
+        fill(hoverFi);
+        stroke(hoverStro);
+        strokeWeight(hoverStroWei);
+    } else {
+        fill(fi);
+        stroke(stro);
+        strokeWeight(stroWei);
+    }
+    rectMode(CENTER);
+    rect(x, y, wid, hei, radius);
+    if(abs(x - mouseX) <= wid / 2 && abs(y - mouseY) <= hei / 2) {
+        fill(hoverTextFill);
+    } else {
+        fill(textFill);
+    }
+    textAlign(CENTER, CENTER);
+    textSize(textSi);
+    text(tex, x, y);
+};
 
 /** --- RENDERING & INPUT --- **/
 var Menu = {
     draw: function() {
         background(0, 0, 0);
+        button(300, 300, 200, 53, 10, "Play", 24, color(0, 0, 0), color(82, 82, 82), color(255, 255, 255), color(200, 200, 200, 100), color(0, 0, 0, 100), 2, color(0, 0, 0, 100), 2);
+        button(300, 360, 200, 53, 10, "options", 24, color(0, 0, 0), color(82, 82, 82), color(255, 255, 255), color(200, 200, 200, 100), color(0, 0, 0, 100), 2, color(0, 0, 0, 100), 2);
+        button(300, 420, 200, 53, 10, "Credits", 24, color(0, 0, 0), color(82, 82, 82), color(255, 255, 255), color(200, 200, 200, 100), color(0, 0, 0, 100), 2, color(0, 0, 0, 100), 2);
     }
 };
 var GameHandler = {
-    currScreen: 0,
+    currScreen: "menu",
     update: function() {
+        // Default Styling
+        noStroke();
+        strokeWeight(1);
+        rectMode(CENTER);
+        textAlign(CENTER, CENTER);
+        textFont(createFont("Star Jedi Rounded"));
+        
         switch(this.currScreen) {
-            case 0:
+            case "menu":
                 Menu.draw();
                 break;
         }
