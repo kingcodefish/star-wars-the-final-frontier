c0c89ccb1151994b00eaa0d5c20c36320c5c6281
KingCodeFish <kingcodefish@gmail.com>
2016-01-13 17:26:15 -0500

    Update font.js

--- Game/font.js
+++ Game/font.js
@@ -1,5 +1,1 @@
-// maybe font "Roboto" works?
-textFont(createFont("Roboto Bold"));
-
-// I can't do the logo "Star Wars" because I'm not the best arc and bezier user. 
-// However, I can do the tilted text for the beginning.
+// We're making a Star Wars font
