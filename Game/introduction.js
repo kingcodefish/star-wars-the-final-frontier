56e8164a0b3a7512102ed85814d800d8b562b368
KingCodeFish <kingcodefish@gmail.com>
2016-01-14 12:15:40 -0500

    Removed Confusion of Author

--- Game/introduction.js
+++ Game/introduction.js
@@ -1,3 +1,4 @@
 // This is the introduction.
 // I know there is no code listed but I am doing most of it on a unsaved file on Khan Academy, on a private account.
 // Expect to see the product tomorrow or day after. 
+// - Indie
