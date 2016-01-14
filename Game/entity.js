319306dd66f5428b154f5503bbec85a5727de9d9
KingCodeFish <kingcodefish@gmail.com>
2016-01-14 13:47:27 -0500

    Update entity.js

--- Game/entity.js
+++ Game/entity.js
@@ -1,3 +1,10 @@
+var collideWith = function(object1, object2) {
+	return (object1.x + object1.width > object2.x &&
+        object1.x + 1 < object2.x + object2.width &&
+        object1.y + object1.height > object2.y &&
+        object1.y < object2.y + object2.height);
+};
+
 var Entity = function(x, y, width, height, bitmap) {
 	this.x = x;
 	this.y = y;
@@ -5,8 +12,6 @@ var Entity = function(x, y, width, height, bitmap) {
 	this.height = height;
 	this.speed = 2.5;
 	this.bitmap = bitmap;
-};
-Entity.prototype.collideWith = function() {
 };
 Entity.prototype.draw = function() {
 	image(this.bitmap, this.x, this.y);
@@ -43,8 +48,8 @@ Player.prototype.update = function() {
         this.y += this.speed;
     }
 
-    for (var each in blocks) {
-        if (collideWith(this, blocks[each])) {
+    for (var i = 0; i < blocks; i++) {
+        if (collideWith(this, blocks[i])) {
             if (keys[UP]) {
                 this.y += this.speed;
             }
